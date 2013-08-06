cb8107602bf27bd29db9f5dd9c6a85d396cf2891
paladin-tech <johnsie@gmail.com>
2013-08-06 11:15:06 +0200

    CSS changes

--- protected/views/selection/index.php
+++ protected/views/selection/index.php
@@ -2,7 +2,8 @@
 <div id="mainContent">
 	<h1>Custom Football Kits</h1>
 	<p>Cvetex manufactures customised team kits of professional quality. Your shirt is created according to your exact specifications. Choose your own design, colours, texts and add any logos. All for no extra cost. Cvetex also offers bespoke football shorts and socks. Click here to see a complete overview of our products.</p>
-	<p class="mainTextKit"><a href="<?= $this->createUrl("selection/catalogue", array("sportId" => "4", 'sex' => 'm')) ?>">Free Design - Kit Designer</a></p>
+	<div class="mainTextKit"><a href="<?= $this->createUrl("selection/catalogue", array("sportId" => "4", 'sex' => 'm')) ?>">Free Design - Kit Designer</a></div>
+	<p>&nbsp;</p>
 	<p>Create your jersey online, quick and easy with the team kit designer.</p>
 </div>
 <div class="clear"></div>
